4e955bfa7f54aeeeffac93aa3c354e2d605d62fa
1fanfasyy <111297132+1fantasyy@users.noreply.github.com>
2024-06-13 03:39:28 +0300

    +comments main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import tkinter as tk
 from views.main_menu import MainMenu
 from database import connect_db
 
+#  Основна функція програми, яка ініціалізує підключення до бази даних,
+#  створює головне вікно програми та запускає головне меню
 def main():
-    connect_db()
-    root = tk.Tk()
-    app = MainMenu(root)
-    root.mainloop()
+    connect_db()  # Встановлює підключення до бази даних
+    root = tk.Tk()  # Створює головне вікно Tkinter
+    app = MainMenu(root)  # Ініціалізує головне меню програми
+    root.mainloop()  # Запускає головний цикл обробки подій Tkinter
 
 if __name__ == "__main__":
     main()
+
